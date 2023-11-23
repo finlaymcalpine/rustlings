fb558e87615242aa890591a5de48eb9f051f1172
Finlay McAlpine <finlay.mcalpine1@googlemail.com>
2023-11-22 22:47:15 -0500

    Vecs & MoveSemantics

--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
 // hint.
 
-const NUMBER: usize = 3; // when we define a constant, we need to give a type declaration. doesn't infer type
+const NUMBER: u32 = 3; // when we define a constant, we need to give a type declaration. doesn't infer type
 fn main() {
     println!("Number {}", NUMBER);
 }
