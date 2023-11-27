14743e6bfa0d3a902ec861b211309706e40830e1
Finlay McAlpine <finlay.mcalpine1@googlemail.com>
2023-11-27 15:45:44 -0500

    Tests & Iterators

--- exercises/17_tests/tests2.rs
+++ exercises/17_tests/tests2.rs
@@ -6,12 +6,12 @@
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
+    const x: u8 = 7;
+    const y: u8 = 7;
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(x, y);
     }
 }
